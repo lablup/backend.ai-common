14109732b9d639de6b3cf978addd8a72b74e4f25
Joongi Kim <joongi@lablup.com>
2015-06-28 00:24:31 +0900

    Build initial package structure.
     * Used namespace package to allow separate installation of sorna.proto

---
+++ setup.py
@@ -0,0 +1,39 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='sorna-protocols',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+    description='Sorna protocols',
+    long_description='',
+    url='https://github.com/lablup/sorna-protocols',
+    author='Lablup Inc.',
+    author_email='joongi@lablup.com',
+    license='Private',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    namespace_packages=['sorna'],
+
+    install_requires=['pyzmq', 'aiozmq', 'namedlist', 'protobuf>=3.0'],
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+    package_data={
+        'protocols': ['agent.proto', 'manager.proto'],
+    },
+    data_files=[],
+
+    entry_points={
+        'console_scripts': [],
+    },
+)
