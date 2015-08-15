433d0e445e0814ca3a0fced0bac5f125e9075250
Joongi Kim <joongi@lablup.com>
2015-08-15 18:12:34 +0900

    refs lablup/sorna#9: Add a common key generator with shorter encoding.

--- sorna/proto/__init__.py
+++ sorna/proto/__init__.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
 
-from .serialize import odict, msg_encode, msg_decode
+from .serialize import odict, msg_encode, msg_decode, generate_uuid
 
-__all__ = ['odict', 'msg_encode', 'msg_decode']
+__all__ = ['odict', 'msg_encode', 'msg_decode', 'generate_uuid']
--- sorna/proto/serialize.py
+++ sorna/proto/serialize.py
@@ -12,3 +12,8 @@ def msg_decode(s):
     if isinstance(s, bytes):
         s = s.decode('utf8')
     return json.loads(s, object_pairs_hook=odict)
+
+def generate_uuid():
+    u = uuid.uuid4()
+    # Strip the last two padding characters because u always has fixed length.
+    return base64.urlsafe_b64encode(u.bytes)[:-2]
