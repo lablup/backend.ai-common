91a42ffb722191742584e3bdf5faf94c4189796a
Joongi Kim <joongi@lablup.com>
2015-10-30 02:19:59 +0900

    Add more methods to retrieve EC2 instance metadata.

--- sorna/utils.py
+++ sorna/utils.py
@@ -2,15 +2,22 @@
 import asyncio
 import aiohttp
 
-async def get_instance_ip():
+async def curl(url, default_value=None):
     try:
-        url = 'http://instance-data/latest/meta-data/local-ipv4'
         resp = await asyncio.wait_for(aiohttp.get(url), 0.2)
     except (asyncio.TimeoutError, aiohttp.errors.ClientOSError):
-        return '127.0.0.1'
+        return default_value
     if resp.status == 200:
         body = await resp.text()
         return body.strip()
     else:
-        return '127.0.0.1'
+        return default_value
 
+async def get_instance_id():
+    return await curl('http://instance-data/latest/meta-data/instance-id', 'i-00000000')
+
+async def get_instance_ip():
+    return await curl('http://instance-data/latest/meta-data/local-ipv4', '127.0.0.1')
+
+async def get_instance_type():
+    return await curl('http://instance-data/latest/meta-data/instance-type', 'unknown')
