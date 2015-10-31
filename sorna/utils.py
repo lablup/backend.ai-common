a58eabd97a790c3f35c81709c6b86c7dcfc74fd1
Joongi Kim <joongi@lablup.com>
2015-10-31 16:45:29 +0900

    Allow explicit loop parameters.

--- sorna/utils.py
+++ sorna/utils.py
@@ -2,22 +2,24 @@
 import asyncio
 import aiohttp
 
-async def curl(url, default_value=None):
+async def curl(url, default_value=None, loop=None):
     try:
-        resp = await asyncio.wait_for(aiohttp.get(url), 0.2)
+        resp = await asyncio.wait_for(aiohttp.get(url, loop=loop),
+                                      timeout=0.2, loop=loop)
+        async with resp:
+            if resp.status == 200:
+                body = await resp.text()
+                return body.strip()
+            else:
+                return default_value
     except (asyncio.TimeoutError, aiohttp.errors.ClientOSError):
         return default_value
-    if resp.status == 200:
-        body = await resp.text()
-        return body.strip()
-    else:
-        return default_value
 
-async def get_instance_id():
-    return await curl('http://instance-data/latest/meta-data/instance-id', 'i-00000000')
+async def get_instance_id(loop=None):
+    return await curl('http://169.254.169.254/latest/meta-data/instance-id', 'i-00000000', loop=loop)
 
-async def get_instance_ip():
-    return await curl('http://instance-data/latest/meta-data/local-ipv4', '127.0.0.1')
+async def get_instance_ip(loop=None):
+    return await curl('http://169.254.169.254/latest/meta-data/local-ipv4', '127.0.0.1', loop=loop)
 
-async def get_instance_type():
-    return await curl('http://instance-data/latest/meta-data/instance-type', 'unknown')
+async def get_instance_type(loop=None):
+    return await curl('http://169.254.169.254/latest/meta-data/instance-type', 'unknown', loop=loop)
