ab030f81439645d466cfbb70551353d3463c150e
Joongi Kim <joongi@lablup.com>
2016-02-14 15:28:00 +0900

    Improve aiohttp-based AWS instance queries and add AsyncBarrier class.
     * No more bogus error messages when calling get_instance_ip() and
       related functions.
    
     * AsyncBarrier emulates threading.Barrier for asyncio event loops.

--- sorna/utils.py
+++ sorna/utils.py
@@ -2,27 +2,54 @@
 import asyncio
 import aiohttp
 
-@asyncio.coroutine
-def curl(url, default_value=None, loop=None):
+async def curl(url, default_value=None, loop=None):
     try:
-        resp = yield from asyncio.wait_for(aiohttp.get(url, loop=loop),
-                                           timeout=0.2, loop=loop)
-        if resp.status == 200:
-            body = yield from resp.text()
-            return body.strip()
-        else:
-            return default_value
+        with aiohttp.Timeout(0.2):
+            resp = await aiohttp.get(url, loop=loop)
+            if resp.status == 200:
+                body = await resp.text()
+                return body.strip()
+            else:
+                return default_value
     except (asyncio.TimeoutError, aiohttp.errors.ClientOSError):
         return default_value
 
-@asyncio.coroutine
-def get_instance_id(loop=None):
-    return (yield from curl('http://169.254.169.254/latest/meta-data/instance-id', 'i-00000000', loop=loop))
+async def get_instance_id(loop=None):
+    return (await curl('http://169.254.169.254/latest/meta-data/instance-id', 'i-00000000', loop=loop))
 
-@asyncio.coroutine
-def get_instance_ip(loop=None):
-    return (yield from curl('http://169.254.169.254/latest/meta-data/local-ipv4', '127.0.0.1', loop=loop))
+async def get_instance_ip(loop=None):
+    return (await curl('http://169.254.169.254/latest/meta-data/local-ipv4', '127.0.0.1', loop=loop))
 
-@asyncio.coroutine
-def get_instance_type(loop=None):
-    return (yield from curl('http://169.254.169.254/latest/meta-data/instance-type', 'unknown', loop=loop))
+async def get_instance_type(loop=None):
+    return (await curl('http://169.254.169.254/latest/meta-data/instance-type', 'unknown', loop=loop))
+
+
+class AsyncBarrier:
+    '''
+    This class provides a simplified asyncio-version of threading.Barrier class.
+    '''
+
+    num_parties = 1
+    loop = None
+    cond = None
+
+    def __init__(self, num_parties, loop=None):
+        self.num_parties = num_parties
+        self.count = 0
+        self.loop = loop if loop else asyncio.get_event_loop()
+        self.cond = asyncio.Condition(loop=self.loop)
+
+    async def wait(self):
+        async with self.cond:
+            self.count += 1
+            if self.count == self.num_parties:
+                self.cond.notify_all()
+            else:
+                while self.count < self.num_parties:
+                    await self.cond.wait()
+
+    def reset(self):
+        self.count = 0
+        # FIXME: if there are waiting coroutines, let them
+        #        raise BrokenBarrierError like threading.Barrier
+
