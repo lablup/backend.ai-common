9eee7347e31be6da0cbd3bc7f5179d676f532ee6
Joongi Kim <joongi@lablup.com>
2016-02-14 22:16:39 +0900

    refs lablup/sorna-manager#14: Add PING request type for agents.

--- sorna/proto/msgtypes.py
+++ sorna/proto/msgtypes.py
@@ -10,11 +10,12 @@ class SornaResponseTypes(enum.IntEnum):
 
 class ManagerRequestTypes(enum.IntEnum):
     PING          = 0  # used for service status monitoring
-    HEARTBEAT     = 1  # sent by agents to inform their
+    HEARTBEAT     = 1  # (unused)
     GET_OR_CREATE = 2
     DESTROY       = 3
 
 class AgentRequestTypes(enum.IntEnum):
+    PING           = 0  # used for service status monitoring
     EXECUTE        = 1
     CREATE_KERNEL  = 2
     DESTROY_KERNEL = 3
