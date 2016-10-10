f393f7bc2a724c20be61061e07777fcaca58c930
Joongi Kim <joongi@lablup.com>
2016-10-10 16:24:48 +0900

    Refactor and extend sorna.argparse utility.
     * Add 'path' argument type that checks filesystem existence of the given
       path string and returns pathlib.Path object for convenience.

--- sorna/argparse.py
+++ sorna/argparse.py
@@ -2,41 +2,57 @@
 
 import argparse
 import ipaddress
+import pathlib
 
 def port_no(s):
     try:
         port = int(s)
         assert port > 0
         assert port < 65536
-    except (ValueError, AssertionError) as e:
-        raise argparse.ArgumentTypeError('The port number should be a positive integer between 0 and 65536.') from e
+    except (ValueError, AssertionError):
+        msg = '{!r} is not a valid port number.'.format(s)
+        raise argparse.ArgumentTypeError(msg)
     return port
 
 def positive_int(s):
     try:
         val = int(s)
         assert val > 0
-    except (ValueError, AssertionError) as e:
-        raise argparse.ArgumentTypeError('The value should be a positive integer.') from e
+    except (ValueError, AssertionError):
+        msg = '{!r} is not a positive integer.'.format(s)
+        raise argparse.ArgumentTypeError(msg)
     return val
 
 def host_port_pair(s):
     pieces = s.split(':', maxsplit=1)
     if len(pieces) == 1:
-        raise argparse.ArgumentTypeError('%r should contain both IP address and port number.', s)
+        msg = '{!r} should contain both IP address and port number.'.format(s)
+        raise argparse.ArgumentTypeError(msg)
     elif len(pieces) == 2:
         ipaddr = pieces[0]
         try:
             port = int(pieces[1])
             assert port > 0
             assert port < 65536
-        except (ValueError, AssertionError) as e:
-            raise argparse.ArgumentTypeError('The port number should be a positive integer between 0 and 65536.') from e
+        except (ValueError, AssertionError):
+            msg = '{!r} is not a valid port number.'.format(s)
+            raise argparse.ArgumentTypeError(msg)
     return (ipaddr, port)
 
 def ipaddr(s):
     try:
         addr = ipaddress.ip_address(s)
     except ValueError as e:
-        raise argparse.ArgumentTypeError('The value is not a valid IP address.') from e
+        msg = '{!r} is not a valid IP address.'.format(s)
+        raise argparse.ArgumentTypeError(msg)
     return addr
+
+def path(val):
+    if val is None:
+        return None
+    p = pathlib.Path(val)
+    if not p.exists():
+        msg = '{!r} is not a valid file/dir path.'.format(val)
+        raise argparse.ArgumentTypeError(msg)
+    return p
+
