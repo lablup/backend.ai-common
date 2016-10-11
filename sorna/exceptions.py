cf4cb42cd75f1291791b5459f7eeb6c4b2894c97
Joongi Kim <joongi@lablup.com>
2016-10-11 13:29:10 +0900

    Improve exception hierarchy and RFC 7807 conformance.

--- sorna/exceptions.py
+++ sorna/exceptions.py
@@ -1,27 +1,53 @@
-#! /usr/bin/env python3
+from .utils import odict
 
 
-class InstanceNotAvailableError(RuntimeError):
-    pass
+class SornaError(RuntimeError):
+    http_status = 500
+    error_id    = 'https://api.sorna.io/probs/general-error'
+    error_title = 'General Sorna API Error.'
 
-class InstanceNotFoundError(RuntimeError):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        self.args = (self.error_id, self.error_title, *self.args)
 
-class KernelNotFoundError(RuntimeError):
-    pass
+    def serialize(self):
+        return odict(('type', self.error_id),
+                     ('title', self.error_title))
 
-class AgentError(RuntimeError):
-    pass
 
-class ManagerError(RuntimeError):
-    pass
+class InstanceNotAvailableError(SornaError):
+    http_status = 503
+    error_id    = 'https://api.sorna.io/probs/instance-not-available'
+    error_title = 'There is no available instance.'
 
-class QuotaExceededError(AgentError):
-    pass
 
-class KernelCreationFailedError(AgentError):
-    pass
+class InstanceNotFoundError(SornaError):
+    http_status = 404
+    error_id    = 'https://api.sorna.io/probs/instance-not-found'
+    error_title = 'No such instance.'
 
-class KernelDestructionFailedError(AgentError):
-    pass
+
+class KernelNotFoundError(SornaError):
+    http_status = 404
+    error_id    = 'https://api.sorna.io/probs/kernel-not-found'
+    error_title = 'No such kernel.'
+
+
+class QuotaExceededError(SornaError):
+    http_status = 412
+    error_id    = 'https://api.sorna.io/probs/quota-exceeded'
+    error_title = 'You have reached your resource limit.'
+
+
+class KernelCreationFailedError(SornaError):
+    http_status = 500
+    error_id    = 'https://api.sorna.io/probs/kernel-ceration-failed'
+    error_title = 'Kernel creation has failed.'
+
+
+class KernelDestructionFailedError(SornaError):
+    http_status = 500
+    error_id    = 'https://api.sorna.io/probs/kernel-destruction-failed'
+    error_title = 'Kernel destruction has failed.'
+
 
