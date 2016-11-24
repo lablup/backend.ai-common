ffeb9291a9b53672c0ea81b94ee5a572b086d276
Joongi Kim <joongi@lablup.com>
2016-11-25 02:55:23 +0900

    Further improve exception hierarchy.

--- sorna/exceptions.py
+++ sorna/exceptions.py
@@ -1,9 +1,13 @@
 '''
-This module defines a series of Sorna-specific errors
-based on HTTP Error classes from aiohttp.
-Raising a SornaError automatically is automatically
-mapped to a corresponding HTTP error with RFC7807-style
-JSON-encoded description in its response body.
+This module defines a series of Sorna-specific errors based on HTTP Error
+classes from aiohttp.
+Raising a SornaError automatically is automatically mapped to a corresponding
+HTTP error response with RFC7807-style JSON-encoded description in its response
+body.
+
+In the client side, you should use "type" field in the body to distinguish
+canonical error types beacuse "title" field may change due to localization and
+future UX improvements.
 '''
 
 from aiohttp import web
@@ -17,18 +21,20 @@ class SornaError(web.HTTPError):
     error_type  = 'https://api.sorna.io/probs/general-error'
     error_title = 'General Sorna API Error.'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
         self.args = (self.status_code, self.reason, self.error_type)
         self.empty_body = False
-        self.body = json.dumps({
-            'type': self.error_type,
-            'title': self.error_title,
-        }).encode()
+        self.content_type = 'application/problem+json'
+        self.body = json.dumps(odict(
+            ('type', self.error_type),
+            ('title', self.error_title),
+        )).encode()
 
-    def serialize(self):
-        return odict(('type', self.error_type),
-                     ('title', self.error_title))
+
+class QueryNotImplemented(web.HTTPServiceUnavailable, SornaError):
+    error_type  = 'https://api.sorna.io/probs/not-implemented'
+    error_title = 'This API query is not implemented.'
 
 
 class InvalidAuthParameters(web.HTTPBadRequest, SornaError):
@@ -41,9 +47,9 @@ class AuthorizationFailed(web.HTTPUnauthorized, SornaError):
     error_title = 'Credential/signature mismatch.'
 
 
-class InstanceNotAvailable(web.HTTPServiceUnavailable, SornaError):
-    error_type  = 'https://api.sorna.io/probs/instance-not-available'
-    error_title = 'There is no available instance.'
+class InvalidAPIParameters(web.HTTPBadRequest, SornaError):
+    error_type  = 'https://api.sorna.io/probs/invalid-api-params'
+    error_title = 'Missing or invalid API parameters.'
 
 
 class InstanceNotFound(web.HTTPNotFound, SornaError):
@@ -66,16 +72,51 @@ class RateLimitExceeded(web.HTTPTooManyRequests, SornaError):
     error_title = 'You have reached your API query rate limit.'
 
 
-class KernelCreationFailed(web.HTTPInternalServerError, SornaError):
-    error_type  = 'https://api.sorna.io/probs/kernel-ceration-failed'
+class InstanceNotAvailable(web.HTTPServiceUnavailable, SornaError):
+    error_type  = 'https://api.sorna.io/probs/instance-not-available'
+    error_title = 'There is no available instance.'
+
+
+class SornaAgentError(SornaError):
+
+    _short_type_map = {
+        'TIMEOUT': 'https://api.sorna.io/probs/agent-timeout',
+        'INVALID_INPUT': 'https://api.sorna.io/probs/agent-invalid-input',
+        'FAILURE': 'https://api.sorna.io/probs/agent-failure',
+    }
+
+    def __init__(self, agent_error_type, agent_error_title=None):
+        super().__init__()
+        if not agent_error_type.startswith('https://'):
+            agent_error_type = self._short_type_map[agent_error_type.upper()]
+        self.agent_error_type = agent_error_type
+        self.agent_error_title = agent_error_title
+        self.args = (self.status_code, self.reason, self.error_type, self.agent_error_type)
+        self.body = json.dumps(odict(
+            ('type', self.error_type),
+            ('title', self.error_title),
+            ('agent-details', odict(
+                ('type', self.agent_error_type),
+                ('title', self.agent_error_title),
+            )),
+        )).encode()
+
+
+class KernelCreationFailed(web.HTTPInternalServerError, SornaAgentError):
+    error_type  = 'https://api.sorna.io/probs/kernel-creation-failed'
     error_title = 'Kernel creation has failed.'
 
 
-class KernelDestructionFailed(web.HTTPInternalServerError, SornaError):
+class KernelDestructionFailed(web.HTTPInternalServerError, SornaAgentError):
     error_type  = 'https://api.sorna.io/probs/kernel-destruction-failed'
     error_title = 'Kernel destruction has failed.'
 
 
-class KernelExecutionFailed(web.HTTPInternalServerError, SornaError):
+class KernelRestartFailed(web.HTTPInternalServerError, SornaAgentError):
+    error_type  = 'https://api.sorna.io/probs/kernel-restart-failed'
+    error_title = 'Kernel restart has failed.'
+
+
+class KernelExecutionFailed(web.HTTPInternalServerError, SornaAgentError):
     error_type  = 'https://api.sorna.io/probs/kernel-execution-failed'
     error_title = 'Executing user code in the kernel has failed.'
