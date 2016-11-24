fa507e3f8ead5e73765c43d4e05cc35f359cf175
Joongi Kim <joongi@lablup.com>
2016-11-24 16:15:13 +0900

    Add a new exception: RateLimitExceeded
     * Remove "Error" suffix from exception names

--- sorna/exceptions.py
+++ sorna/exceptions.py
@@ -15,43 +15,49 @@ class SornaError(RuntimeError):
                      ('title', self.error_title))
 
 
-class InstanceNotAvailableError(SornaError):
+class InstanceNotAvailable(SornaError):
     http_status = 503
     error_id    = 'https://api.sorna.io/probs/instance-not-available'
     error_title = 'There is no available instance.'
 
 
-class InstanceNotFoundError(SornaError):
+class InstanceNotFound(SornaError):
     http_status = 404
     error_id    = 'https://api.sorna.io/probs/instance-not-found'
     error_title = 'No such instance.'
 
 
-class KernelNotFoundError(SornaError):
+class KernelNotFound(SornaError):
     http_status = 404
     error_id    = 'https://api.sorna.io/probs/kernel-not-found'
     error_title = 'No such kernel.'
 
 
-class QuotaExceededError(SornaError):
+class QuotaExceeded(SornaError):
     http_status = 412
     error_id    = 'https://api.sorna.io/probs/quota-exceeded'
     error_title = 'You have reached your resource limit.'
 
 
-class KernelCreationFailedError(SornaError):
+class RateLimitExceeded(SornaError):
+    http_status = 429
+    error_id    = 'https://api.sorna.io/probs/rate-limit-exceeded'
+    error_title = 'You have reached your API query rate limit.'
+
+
+class KernelCreationFailed(SornaError):
     http_status = 500
     error_id    = 'https://api.sorna.io/probs/kernel-ceration-failed'
     error_title = 'Kernel creation has failed.'
 
 
-class KernelDestructionFailedError(SornaError):
+class KernelDestructionFailed(SornaError):
     http_status = 500
     error_id    = 'https://api.sorna.io/probs/kernel-destruction-failed'
     error_title = 'Kernel destruction has failed.'
 
 
-class KernelExecutionFailedError(SornaError):
+class KernelExecutionFailed(SornaError):
     http_status = 500
     error_id    = 'https://api.sorna.io/probs/kernel-execution-failed'
     error_title = 'Executing user code in the kernel has failed.'
