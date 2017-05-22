1a0b55d58307b9dc324f1d6eaec8220e6a2fbcb9
Joongi Kim <joongi@lablup.com>
2017-05-22 17:44:20 +0900

    Update msgbus and its tests to recognize env-vars
     * For consistent testing experience in local and travis

--- tests/test_msgbus.py
+++ tests/test_msgbus.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 import pytest
 
@@ -6,8 +7,18 @@ from sorna.argparse import host_port_pair
 from sorna.msgbus import MsgChannel
 
 
+@pytest.fixture
+async def channel():
+    addr = os.environ.get('SORNA_MQ_ADDR', 'localhost:5682')
+    c = MsgChannel(host_port_pair(addr))
+    await c.init(
+        os.environ.get('SORNA_MQ_LOGIN', 'sorna'),
+        os.environ.get('SORNA_MQ_PASS', 'develove'),
+        os.environ.get('SORNA_NAMESPACE', 'local'))
+    return c
+
+
 @pytest.mark.asyncio
-async def test_channel():
-    c = MsgChannel(host_port_pair('localhost:5682'))
-    await c.init('sorna', 'develove', 'local')
+async def test_channel(channel):
+    c = await channel
     await c.close()
