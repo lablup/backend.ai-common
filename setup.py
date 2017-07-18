d02bd7dc489dd9e82c772cba955765cc1a8bddea
Joongi Kim <joongi@lablup.com>
2017-07-18 23:24:06 +0900

    Set version to 0.9.5

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0a1',
+    version='0.9.5',
     description='Sorna common libraries',
     long_description=long_description,
     url='https://github.com/lablup/sorna-common',
