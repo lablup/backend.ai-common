50000354ad8001cc2b3ee41f67ab99e746a0c1bf
Joongi Kim <joongi@lablup.com>
2017-10-13 15:59:37 +0900

    Bump version to 1.0.1 for next release

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
     description='Backend.AI commons library',
     long_description=Path('README.rst').read_text(),
     url='https://github.com/lablup/backend.ai-common',
