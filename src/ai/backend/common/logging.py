a8e23b9ec95a4a3d98651ebfb0eccd477eecf030
Joongi Kim <joongi@lablup.com>
2018-10-25 16:43:49 +0900

    Add a brace-style message adapter for logging (#2)
    * Users can import BraceStyleAdapter and wrap the standard logger with
      it to make it effective.

--- src/ai/backend/common/logging.py
+++ src/ai/backend/common/logging.py
@@ -108,6 +108,29 @@ def log_worker(config, log_queue):
         file_handler.emit(rec)
 
 
+class BraceMessage:
+
+    __slots__ = ('fmt', 'args')
+
+    def __init__(self, fmt, args):
+        self.fmt = fmt
+        self.args = args
+
+    def __str__(self):
+        return self.fmt.format(*self.args)
+
+
+class BraceStyleAdapter(logging.LoggerAdapter):
+
+    def __init__(self, logger, extra=None):
+        super().__init__(logger, extra)
+
+    def log(self, level, msg, *args, **kwargs):
+        if self.isEnabledFor(level):
+            msg, kwargs = self.process(msg, kwargs)
+            self.logger._log(level, BraceMessage(msg, args), (), **kwargs)
+
+
 class Logger():
 
     def __init__(self, config):
