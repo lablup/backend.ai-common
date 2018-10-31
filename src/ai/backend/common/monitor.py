614a6843b3e6a33b74e05624a6cf12696e1015cc
Suhwan Jee <suhwan.jee@lablup.com>
2018-10-31 10:49:55 +0900

    Change interface of monitoring tools

--- src/ai/backend/common/monitor.py
+++ src/ai/backend/common/monitor.py
@@ -1,27 +1,20 @@
 '''
 A collection of dummy monitoring daemon classes with no-op stubs.
-Useful to avoid excessive repetition of "if app['datadog']:
-app['datadog'].statsd.increment(...)" snippets all scattered around the
+Useful to avoid excessive repetition of "if app['stats_monitor']:
+app['stats_monitor'].statsd.increment(...)" snippets all scattered around the
 codebase.
 '''
 
 
-class DummyStatsd:
-    def increment(self, *args, **kwargs): pass  # noqa
-    def event(self, *args, **kwargs): pass      # noqa
-    def gauge(self, *args, **kwargs): pass      # noqa
+class DummyStatsMonitor:
+    def report_stats(self, *args, **kwargs): pass        # noqa
     def __enter__(self): return self                     # noqa
     def __exit__(self, exc_type, exc_val, exc_tb): pass  # noqa
 
 
-class DummyDatadog:
-    statsd = DummyStatsd()
-
-
-class DummySentry:
-    def captureException(self, *args, **kwargs): pass  # noqa
-    def captureMessage(self, *args, **kwargs): pass    # noqa
-    def capture(self, *args, **kwargs): pass           # noqa
-    def user_context(self, context): pass              # noqa
+class DummyErrorMonitor:
+    def capture_exception(self, *args, **kwargs): pass   # noqa
+    def capture_message(self, *args, **kwargs): pass     # noqa
+    def set_context(self, context): pass                 # noqa
     def __enter__(self): return self                     # noqa
     def __exit__(self, exc_type, exc_val, exc_tb): pass  # noqa
