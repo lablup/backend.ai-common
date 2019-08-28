86963074528c58dbe6b0291f4e1f251162c5645d
Jonghyun Park <adrysn@gmail.com>
2019-08-28 19:42:13 +0900

    Add utility classes for playing with /etc/fstab file

--- src/ai/backend/common/utils.py
+++ src/ai/backend/common/utils.py
@@ -61,7 +61,7 @@ def generate_uuid():
 def nmget(o, key_path, def_val=None, path_delimiter='.', null_as_default=True):
     '''
     A short-hand for retrieving a value from nested mappings
-    ("nested-mapping-get").  At each level it checks if the given "path"
+    ("nested-mapping-get"). At each level it checks if the given "path"
     component in the given key exists and return the default value whenever
     fails.
 
@@ -194,3 +194,99 @@ class AsyncBarrier:
         self.count = 0
         # FIXME: if there are waiting coroutines, let them
         #        raise BrokenBarrierError like threading.Barrier
+
+
+class FstabEntry:
+    """
+    Entry class represents a non-comment line on the `fstab` file.
+    """
+    def __init__(self, device, mountpoint, fstype, options, d=0, p=0):
+        self.device = device
+        self.mountpoint = mountpoint
+        self.fstype = fstype
+        if not options:
+            options = 'defaults'
+        self.options = options
+        self.d = d
+        self.p = p
+
+    def __eq__(self, o):
+        return str(self) == str(o)
+
+    def __str__(self):
+        return "{} {} {} {} {} {}".format(self.device,
+                                          self.mountpoint,
+                                          self.fstype,
+                                          self.options,
+                                          self.d,
+                                          self.p)
+
+
+class Fstab:
+    """
+    Reader/writer for fstab file.
+    Takes aiofile pointer for async I/O. It should be writable if add/remove
+    operations are needed.
+
+    NOTE: This class references Jorge Niedbalski R.'s gist snippet.
+          We have been converted it to be compatible with Python 3
+          and to support async I/O.
+          (https://gist.github.com/niedbalski/507e974ed2d54a87ad37)
+    """
+    def __init__(self, fp):
+        self._fp = fp
+
+    def _hydrate_entry(self, line):
+        return FstabEntry(*[x for x in line.strip('\n').split(' ') if x not in ('', None)])
+
+    async def get_entries(self):
+        await self._fp.seek(0)
+        while True:
+            try:
+                line = await self._fp.readline()
+                if not line.startswith('#'):
+                    yield self._hydrate_entry(line)
+            except ValueError:
+                pass
+            if not line:
+                break
+
+    async def get_entry_by_attr(self, attr, value):
+        for entry in await self.get_entries():
+            e_attr = getattr(entry, attr)
+            if e_attr == value:
+                return entry
+        return None
+
+    async def add_entry(self, entry):
+        if await self.get_entry_by_attr('device', entry.device):
+            return False
+        await self._fp.write(str(entry) + '\n')
+        await self._fp.truncate()
+        return entry
+
+    async def add(cls, device, mountpoint, fstype, options=None, d=0, p=0):
+        return await self.add_entry(FstabEntry(device, mountpoint, fstype, options, d, p))
+
+    async def remove_entry(self, entry):
+        await self._fp.seek(0)
+        lines = await self._fp.readlines()
+        found = False
+        for index, line in enumerate(lines):
+            if not line.startswith("#"):
+                if self._hydrate_entry(line) == entry:
+                    found = True
+                    break
+        if not found:
+            return False
+        lines.remove(line)
+        await self._fp.seek(0)
+        await self._fp.write(''.join(lines))
+        await self._fp.truncate()
+        return True
+
+    async def remove_by_mountpoint(self, mountpoint):
+        entry = await self.get_entry_by_attr('mountpoint', mountpoint)
+        if entry:
+            return await self.remove_entry(entry)
+        return False
