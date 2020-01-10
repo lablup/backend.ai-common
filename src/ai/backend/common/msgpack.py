ac68846668669a3e0d27b8d9097f38b4a039cfe6
Joongi Kim <joongi@lablup.com>
2020-01-10 15:45:04 +0900

    msgpack: Add typing

--- src/ai/backend/common/msgpack.py
+++ src/ai/backend/common/msgpack.py
@@ -2,12 +2,14 @@
 Wrapper of msgpack-python with good defaults.
 '''
 
+from typing import Any
+
 import msgpack as _msgpack
 
 
-def packb(data, **kwargs):
+def packb(data: Any, **kwargs) -> bytes:
     return _msgpack.packb(data, use_bin_type=True, **kwargs)
 
 
-def unpackb(packed, **kwargs):
+def unpackb(packed: bytes, **kwargs) -> Any:
     return _msgpack.unpackb(packed, raw=False, use_list=False, **kwargs)
