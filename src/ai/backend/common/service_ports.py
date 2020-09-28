f679ccfdee222e3c0ba39c8b35403d6bcfd98e83
Joongi Kim <joongi@lablup.com>
2020-09-28 13:10:03 +0900

    fix: Move tests for service-port parser and improve it (#52)

--- src/ai/backend/common/service_ports.py
+++ src/ai/backend/common/service_ports.py
@@ -1,6 +1,9 @@
 import re
 from typing import (
-    Sequence, List, Set, Any
+    List,
+    Sequence,
+    Set,
+    Type,
 )
 
 from .types import ServicePort, ServicePortProtocols
@@ -13,8 +16,10 @@ _rx_service_ports = re.compile(
     r'^(?P<name>[\w-]+):(?P<proto>\w+):(?P<ports>\[\d+(?:,\d+)*\]|\d+)(?:,|$)')
 
 
-def parse_service_ports(s: str, exception: Any) -> Sequence[ServicePort]:
+def parse_service_ports(s: str, exception_cls: Type[Exception] = None) -> Sequence[ServicePort]:
     items: List[ServicePort] = []
+    if exception_cls is None:
+        exception_cls = ValueError
     used_ports: Set[int] = set()
     while True:
         match = _rx_service_ports.search(s)
@@ -22,22 +27,25 @@ def parse_service_ports(s: str, exception: Any) -> Sequence[ServicePort]:
             s = s[len(match.group(0)):]
             name = match.group('name')
             if not name:
-                raise exception('Service port name must be not empty.')
+                raise exception_cls('Service port name must be not empty.')
             protocol = match.group('proto')
             if protocol == 'pty':
                 # unsupported, skip
                 continue
             if protocol not in ('tcp', 'http', 'preopen'):
-                raise exception(f'Unsupported service port protocol: {protocol}')
+                raise exception_cls(f'Unsupported service port protocol: {protocol}')
             ports = tuple(map(int, match.group('ports').strip('[]').split(',')))
             for p in ports:
                 if p in used_ports:
-                    raise exception(f'The port {p} is already used by another service port.')
+                    raise exception_cls(f'The port {p} is already used by another service port.')
+                if p <= 1024:
+                    raise exception_cls(f'The service port number {p} must be '
+                                        f'larger than 1024 to run without the root privilege.')
                 if p >= 65535:
-                    raise exception(f'The service port number {p} must be smaller than 65535.')
+                    raise exception_cls(f'The service port number {p} must be smaller than 65535.')
                 if p in (2000, 2001, 2002, 2003, 2200, 7681):
-                    raise exception('The service ports 2000 to 2003, 2200 and 7681 '
-                                       'are reserved for internal use.')
+                    raise exception_cls('The service ports 2000 to 2003, 2200 and 7681 '
+                                        'are reserved for internal use.')
                 used_ports.add(p)
             items.append({
                 'name': name,
@@ -46,7 +54,7 @@ def parse_service_ports(s: str, exception: Any) -> Sequence[ServicePort]:
                 'host_ports': (None,) * len(ports),
             })
         else:
-            break
-        if not s:
+            if len(s) > 0:
+                raise exception_cls('Invalid format')
             break
     return items
