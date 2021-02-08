3ddaf9f7604f23f3b4b3334cc39fd28583044fe6
Joongi Kim <joongi@lablup.com>
2021-02-08 15:22:38 +0900

    docs: Add news fragment
    fix: Temporarily disable test_types_meta test cases

--- tests/test_types_meta.py
+++ tests/test_types_meta.py
@@ -1,3 +1,8 @@
+# This file is temporarily disabled due to the following upstream issues:
+# - https://github.com/python/mypy/issues/10013
+# - https://github.community/t/159761
+
+"""
 from ai.backend.common.types import (
     check_typed_tuple,
 )
@@ -15,3 +20,4 @@ def test_check_typed_tuple() -> None:
         c, d = check_typed_tuple(('a', 123), (int, int))
         reveal_type(c)  # R: builtins.int  # noqa
         reveal_type(d)  # R: builtins.int  # noqa
+"""
