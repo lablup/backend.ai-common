fa8c54e667fa121ea5919c8cad711667e8df4af0
Joongi Kim <joongi@lablup.com>
2021-03-25 11:15:20 +0900

    feat: Include msec in the console logs for debugging delay issues

--- src/ai/backend/common/logging.py
+++ src/ai/backend/common/logging.py
@@ -8,6 +8,7 @@ import os
 from pathlib import Path
 import pickle
 import pprint
+import time
 from typing import (
     Any, Optional,
     Mapping, MutableMapping,
@@ -16,6 +17,7 @@ import socket
 import ssl
 import sys
 
+import coloredlogs
 from pythonjsonlogger.jsonlogger import JsonFormatter
 import trafaret as t
 from tblib import pickling_support
@@ -163,6 +165,28 @@ class LogstashHandler(logging.Handler):
             self._sock.sendall(json.dumps(log).encode('utf-8'))
 
 
+class ConsoleFormatter(logging.Formatter):
+
+    def formatTime(self, record: logging.LogRecord, datefmt: str = None) -> str:
+        ct = self.converter(record.created)
+        if datefmt:
+            return time.strftime(datefmt, ct)
+        else:
+            t = time.strftime("%Y-%m-%d %H:%M:%S", ct)
+            return f"{t}.{int(record.msecs):03d}"
+
+
+class ColoredConsoleFormatter(coloredlogs.ColoredFormatter):
+
+    def formatTime(self, record: logging.LogRecord, datefmt: str = None) -> str:
+        ct = self.converter(record.created)
+        if datefmt:
+            return time.strftime(datefmt, ct)
+        else:
+            t = time.strftime("%Y-%m-%d %H:%M:%S", ct)
+            return f"{t}.{int(record.msecs):03d}"
+
+
 class CustomJsonFormatter(JsonFormatter):
 
     def add_fields(self, log_record, record, message_dict):
@@ -206,8 +230,7 @@ def log_worker(
         drv_config = daemon_config['console']
         console_formatter: logging.Formatter
         if drv_config['colored']:
-            import coloredlogs
-            console_formatter = coloredlogs.ColoredFormatter(
+            console_formatter = ColoredConsoleFormatter(
                 log_formats[drv_config['format']],
                 field_styles={'levelname': {'color': 248, 'bold': True},
                               'name': {'color': 246, 'bold': False},
@@ -223,7 +246,7 @@ def log_worker(
                               'critical': {'background': 'red', 'color': 255, 'bold': True}},
             )
         else:
-            console_formatter = logging.Formatter(
+            console_formatter = ConsoleFormatter(
                 log_formats[drv_config['format']],
             )
         console_handler = logging.StreamHandler(
