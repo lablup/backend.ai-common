ed4d567f841340f3612e34a215c007d19311f77f
Joongi Kim <joongi@lablup.com>
2021-05-31 18:36:53 +0900

    fix: Allow overriding msgpack options (#79)
    Backported-From: main
    Backported-To: 21.03

--- src/ai/backend/common/msgpack.py
+++ src/ai/backend/common/msgpack.py
@@ -8,8 +8,10 @@ import msgpack as _msgpack
 
 
 def packb(data: Any, **kwargs) -> bytes:
-    return _msgpack.packb(data, use_bin_type=True, **kwargs)
+    opts = {"use_bin_type": True, **kwargs}
+    return _msgpack.packb(data, **opts)
 
 
 def unpackb(packed: bytes, **kwargs) -> Any:
-    return _msgpack.unpackb(packed, raw=False, use_list=False, **kwargs)
+    opts = {"raw": False, "use_list": False, **kwargs}
+    return _msgpack.unpackb(packed, **opts)
--- tests/test_msgpack.py
+++ tests/test_msgpack.py
@@ -12,3 +12,18 @@ def test_msgpack_with_unicode():
 
     # We also use tuples when unpacking for performance.
     assert unpacked == tuple(data)
+
+
+def test_msgpack_kwargs():
+    x = {'cpu': [0.42, 0.44], 'cuda_mem': [0.0, 0.0], 'cuda_util': [0.0, 0.0], 'mem': [30.0, 30.0]}
+    packed = msgpack.packb(x)
+    unpacked = msgpack.unpackb(packed, use_list=False)
+    assert isinstance(unpacked['cpu'], tuple)
+    assert isinstance(unpacked['mem'], tuple)
+    assert isinstance(unpacked['cuda_mem'], tuple)
+    assert isinstance(unpacked['cuda_util'], tuple)
+    unpacked = msgpack.unpackb(packed, use_list=True)
+    assert isinstance(unpacked['cpu'], list)
+    assert isinstance(unpacked['mem'], list)
+    assert isinstance(unpacked['cuda_mem'], list)
+    assert isinstance(unpacked['cuda_util'], list)
