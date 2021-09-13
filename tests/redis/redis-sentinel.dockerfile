d78428a6d56d9a1989d0b1c4352e44e9165637a7
Joongi Kim <joongi@lablup.com>
2021-09-14 01:07:05 +0900

    wip: Make the cluster working (on Docker for Mac)

--- tests/redis/redis-sentinel.dockerfile
+++ tests/redis/redis-sentinel.dockerfile
@@ -3,7 +3,7 @@ FROM redis:6-alpine
 COPY ./sentinel.conf /etc/redis-sentinel.conf
 
 CMD sed -i'' "s/REDIS_PASSWORD/${REDIS_PASSWORD}/g" /etc/redis-sentinel.conf; \
-    redis-server /etc/redis-sentinel.conf --sentinel
+    redis-server /etc/redis-sentinel.conf --sentinel --port ${REDIS_PORT}
 
 
 # vim: ft=dockerfile
--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,10 +1,13 @@
 import asyncio
 from pathlib import Path
 from typing import (
-    Sequence,
     AsyncIterator,
+    Sequence,
+    Tuple,
 )
 
+import aioredis
+import aioredis.sentinel
 import pytest
 
 from ai.backend.common import redis
@@ -36,24 +39,37 @@ async def redis_container(test_ns) -> AsyncIterator[str]:
 
 
 @pytest.fixture
-async def redis_cluster(test_ns) -> AsyncIterator[None]:
+async def redis_cluster(test_ns) -> AsyncIterator[Sequence[Tuple[str, int]]]:
     cfg_dir = Path(__file__).parent / 'redis'
     await simple_run_cmd([
         'docker-compose',
         '-p', test_ns,
         '-f', str(cfg_dir / 'redis-cluster.yml'),
         'up', '-d',
-    ])
+    ], stdout=asyncio.subprocess.DEVNULL, stderr=asyncio.subprocess.DEVNULL)
+    print()
     await asyncio.sleep(0.2)
+    await simple_run_cmd([
+        'docker-compose',
+        '-p', test_ns,
+        '-f', str(cfg_dir / 'redis-cluster.yml'),
+        'ps',
+    ])
+    await simple_run_cmd(['docker', 'logs', f'{test_ns}_backendai-half-redis-sentinel01_1'])
+    print()
     try:
-        yield
+        yield [
+            ('127.0.0.1', 26379),
+            ('127.0.0.1', 26380),
+            ('127.0.0.1', 26381),
+        ]
     finally:
         await simple_run_cmd([
             'docker-compose',
             '-p', test_ns,
             '-f', str(cfg_dir / 'redis-cluster.yml'),
             'down',
-        ])
+        ], stdout=asyncio.subprocess.DEVNULL, stderr=asyncio.subprocess.DEVNULL)
 
 
 @pytest.mark.asyncio
@@ -73,10 +89,49 @@ async def test_blist(redis_container):
 
 
 @pytest.mark.asyncio
-async def test_pubsub_cluster(redis_cluster):
-    pass
+async def test_connect_cluster_haproxy(redis_cluster):
+    try:
+        await redis.connect_with_retries(
+            url='redis://localhost:9379',
+            password='develove',
+        )
+    except aioredis.exceptions.AuthenticationError:
+        pass
+    r = await redis.connect_with_retries(
+        url='redis://localhost:9379',
+        password='develove',
+    )
+    await r.ping()
+
+
+# @pytest.mark.asyncio
+# async def test_pubsub_cluster_haproxy(redis_cluster):
+#     pass
+#
+#
+# @pytest.mark.asyncio
+# async def test_blist_cluster_haproxy(redis_cluster):
+#     pass
 
 
 @pytest.mark.asyncio
-async def test_blist_cluster(redis_cluster):
-    pass
+async def test_connect_cluster_sentinel(redis_cluster):
+    s = aioredis.sentinel.Sentinel(
+        redis_cluster,
+        password='develove',
+        socket_timeout=0.5,
+    )
+    master = s.master_for('mymaster', db=9)
+    await master.ping()
+    slave = s.slave_for('mymaster', db=9)
+    await slave.ping()
+
+
+# @pytest.mark.asyncio
+# async def test_pubsub_cluster_sentinel(redis_cluster):
+#     pass
+#
+#
+# @pytest.mark.asyncio
+# async def test_blist_cluster_sentinel(redis_cluster):
+#     pass
