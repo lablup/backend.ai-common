06ce1cbc3b065883ec80e2cb68cc989ebb08ba34
Joongi Kim <joongi@lablup.com>
2021-09-14 12:37:10 +0900

    test: Figure out how to properly recover subscribe channels

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -5,12 +5,16 @@ import shutil
 import sys
 from typing import (
     AsyncIterator,
+    List,
     Sequence,
     Tuple,
 )
 
 import aioredis
+import aioredis.client
+import aioredis.exceptions
 import aioredis.sentinel
+import aiotools
 import pytest
 
 from ai.backend.common import redis
@@ -30,7 +34,7 @@ async def redis_container(test_ns) -> AsyncIterator[str]:
         '--name', f'bai-common.{test_ns}',
         '-p', '9379:6379',
         'redis:6-alpine',
-    ], stdout=asyncio.subprocess.PIPE)
+    ], stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.DEVNULL)
     assert p.stdout is not None
     stdout = await p.stdout.read()
     await p.wait()
@@ -95,23 +99,93 @@ async def redis_cluster(test_ns) -> AsyncIterator[Sequence[Tuple[str, int]]]:
 
 
 @pytest.mark.asyncio
-async def test_connect(redis_container):
+async def test_connect(redis_container: str) -> None:
     r = await redis.connect_with_retries(url='redis://localhost:9379')
     await r.ping()
 
 
 @pytest.mark.asyncio
-async def test_pubsub(redis_container):
+async def test_pubsub(redis_container: str) -> None:
+    do_pause = asyncio.Event()
+    paused = asyncio.Event()
+    do_unpause = asyncio.Event()
+    unpaused = asyncio.Event()
+    STOPWORD = "STOP"
+    received_messages: List[str] = []
+
+    async def interrupt() -> None:
+        await do_pause.wait()
+        await simple_run_cmd(['docker', 'stop', redis_container])
+        paused.set()
+        await do_unpause.wait()
+        await simple_run_cmd(['docker', 'start', redis_container])
+        # The pub-sub channel may loose some messages while starting up.
+        # Make a pause here to wait until the container actually begins to listen.
+        await asyncio.sleep(0.5)
+        unpaused.set()
+
+    async def subscribe(pubsub: aioredis.client.PubSub) -> None:
+        while True:
+            try:
+                message = await pubsub.get_message(ignore_subscribe_messages=True, timeout=1.0)
+                if message is not None:
+                    data = message["data"].decode()
+                    if data == STOPWORD:
+                        break
+                    received_messages.append(data)
+            except (RuntimeError, aioredis.exceptions.ConnectionError):
+                await asyncio.sleep(0.3)
+                pubsub.connection = None
+                try:
+                    await pubsub.ping()
+                except aioredis.exceptions.ConnectionError:
+                    pass
+                else:
+                    assert pubsub.connection is not None
+                    await pubsub.on_connect(pubsub.connection)
+                continue
+            except asyncio.CancelledError:
+                break
+
+    r = await redis.connect_with_retries(url='redis://localhost:9379', socket_timeout=0.5)
+    pubsub = r.pubsub()
+    await pubsub.subscribe("ch1")
+
+    subscribe_task = asyncio.create_task(subscribe(pubsub))
+    interrupt_task = asyncio.create_task(interrupt())
+    await asyncio.sleep(0)
+
+    for i in range(5):
+        await r.publish("ch1", str(i))
+        await asyncio.sleep(0.1)
+    do_pause.set()
+    await paused.wait()
+    for i in range(5):
+        # The Redis server is dead temporarily...
+        with pytest.raises(aioredis.exceptions.ConnectionError):
+            await r.publish("ch1", str(5 + i))
+        await asyncio.sleep(0.1)
+    do_unpause.set()
+    await unpaused.wait()
+    for i in range(5):
+        await r.publish("ch1", str(10 + i))
+        await asyncio.sleep(0.1)
+
+    await interrupt_task
+    await r.publish("ch1", STOPWORD)
+    await subscribe_task
+
+    assert [*map(int, received_messages)] == [*range(0, 5), *range(10, 15)]
+
+
+
+@pytest.mark.asyncio
+async def test_blist(redis_container) -> None:
     pass
 
 
 @pytest.mark.asyncio
-async def test_blist(redis_container):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_connect_cluster_haproxy(redis_cluster):
+async def test_connect_cluster_haproxy(redis_cluster) -> None:
     try:
         await redis.connect_with_retries(
             url='redis://localhost:9379',
