97ae1921510e38707c429cdedec9e66acf9fbfe2
Kyujin Cho <kyujin.cho@lablup.com>
2021-10-26 18:26:28 +0900

    Add test case for get_redis_object

--- tests/redis/test_connect.py
+++ tests/redis/test_connect.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+from typing import TYPE_CHECKING
 
 import aioredis
 import aioredis.client
@@ -12,6 +13,11 @@ import pytest
 from .types import RedisClusterInfo
 from .utils import interrupt, with_timeout
 
+from ai.backend.common import redis, validators as tx
+
+if TYPE_CHECKING:
+    from typing import Any
+
 
 @pytest.mark.asyncio
 async def test_connect(redis_container: str) -> None:
@@ -20,6 +26,37 @@ async def test_connect(redis_container: str) -> None:
         socket_timeout=0.5,
     )
     await r.ping()
+
+
+@pytest.mark.asyncio
+async def test_instantiate_redisconninfo() -> None:
+    sentinels = '127.0.0.1:26379,127.0.0.1:26380,127.0.0.1:26381'
+    r1 = redis.get_redis_object({
+        'sentinel': sentinels,
+        'service_name': 'mymaster',
+        'password': 'develove',
+    })
+
+    assert isinstance(r1.client, aioredis.sentinel.Sentinel)
+
+    for i in range(3):
+        assert r1.client.sentinels[i].connection_pool.connection_kwargs['host'] == '127.0.0.1'
+        assert r1.client.sentinels[i].connection_pool.connection_kwargs['port'] == (26379 + i)
+        assert r1.client.sentinels[i].connection_pool.connection_kwargs['db'] == 0
+
+    parsed_addresses: Any = tx.DelimiterSeperatedList(tx.HostPortPair).check_and_return(sentinels)
+    r2 = redis.get_redis_object({
+        'sentinel': parsed_addresses,
+        'service_name': 'mymaster',
+        'password': 'develove',
+    })
+
+    assert isinstance(r2.client, aioredis.sentinel.Sentinel)
+
+    for i in range(3):
+        assert r2.client.sentinels[i].connection_pool.connection_kwargs['host'] == '127.0.0.1'
+        assert r2.client.sentinels[i].connection_pool.connection_kwargs['port'] == (26379 + i)
+        assert r2.client.sentinels[i].connection_pool.connection_kwargs['db'] == 0
 
 
 @pytest.mark.asyncio
