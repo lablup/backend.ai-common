bf531a2a8e61fa0719c284e7f73b4889ded2da26
Joongi Kim <joongi@lablup.com>
2022-04-06 04:58:13 +0000

    wip: Add TODO

--- src/ai/backend/common/lock.py
+++ src/ai/backend/common/lock.py
@@ -127,6 +127,7 @@ class EtcdLock(AbstractDistributedLock):
     ) -> None:
         super().__init__(lifetime=lifetime)
         _timeout = timeout if timeout is not None else self.default_timeout
+        # TODO: extend with_lock() to support key-based lease for lifetime support
         self._con_mgr = etcd.etcd.with_lock(lock_name, timeout=_timeout)
         self._debug = debug
 
